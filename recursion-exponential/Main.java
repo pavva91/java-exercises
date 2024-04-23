3e6018399da7e892573e3b79bd8945bba8f89765
pavva91 <valerio.mattioli@proton.me>
2024-04-24 01:25:28 +0200

    Recursion Exponential

---
+++ recursion-exponential/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int base = Integer.parseInt(args[0]);
+        int exponent = Integer.parseInt(args[1]);
+        int result = exponential(base, exponent);
+        System.out.println(result);
+    }
+
+    static int exponential(int base, int exponent) {
+
+        if (exponent == 0) {
+            return 1;
+        }
+
+        if (exponent == 1) {
+            return base;
+        }
+
+        return base * exponential(base, exponent - 1);
+    }
+    
+}
